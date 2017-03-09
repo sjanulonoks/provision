6801c0e2155f36804ecd2557eebfac4263449828
Greg Althaus <galthaus@austin.rr.com>
2017-03-09 13:14:42 -0600

    Do have a permanent ALL-LICENSE.  The build will build all the right copies.

--- tools/build-all-license.sh
+++ tools/build-all-license.sh
@@ -6,13 +6,20 @@ if [[ $1 != "" ]] ; then
 fi
 
 echo
+echo "Rocket-skates License"
+echo
+
+cat LICENSE.rst
+
+echo
 echo "TODO: Get the downloaded Assets Licenses"
 echo
 
 cd vendor
 find . | grep LICENSE | while read line ; do
     echo
-    echo "GO Package: $line"
+    echo "GO Package License: $line"
+    echo
     cat $line
 done
 cd ..
