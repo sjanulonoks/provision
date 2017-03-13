db4db0fab7c62c3f9872bafba7d98a66630b8ad2
Rob @zehicle Hirschfeld <rob@rackn.com>
2017-03-13 11:45:24 -0500

    correct issues from @vlowther

--- midlayer/dhcp_server.go
+++ midlayer/dhcp_server.go
@@ -10,9 +10,9 @@ import (
 
 func RunDhcpHandler(dhcpInfo *backend.DataTracker, ifs []*backend.Interface) {
 	handlers := make(map[int]dhcp.Handler, 0)
-	log.Println("DOCUMENTATION http://rocket-skates.readthedocs.io/en/latest/doc/faq-troubleshooting.html")
+	dhcpInfo.Logger.Println("DOCUMENTATION - http://rocket-skates.readthedocs.io/en/latest/doc/faq-troubleshooting.html")
 	for _, ii := range ifs {
-		log.Println("Starting on interface: ", ii.Name, " with server ip: ", ii.ActiveAddress)
+		dhcpInfo.Logger.Println("Starting on interface: ", ii.Name, " with server ip: ", ii.ActiveAddress)
 
 		serverIP, _, _ := net.ParseCIDR(ii.ActiveAddress)
 		serverIP = serverIP.To4()
