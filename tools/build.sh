6e0898faa8008253619584e6dbe4099acf67c4d5
Victor Lowther <victor.lowther@gmail.com>
2017-03-13 11:40:16 -0500

    Clean up build.sh
    Stop doing weird semi-recursive checkouts in an effort to not pollute
    someone else's GOPATH.
    
    Instead, just install the tools we need, and trust glide to handle
    vendoring of the stuff we care about.

--- tools/build.sh
+++ tools/build.sh
@@ -1,63 +1,43 @@
 #!/bin/bash
 
-GO_TARGET_VER_MAJOR=1
-GO_TARGET_VER_MINOR=8
+[[ $GOPATH ]] || export GOPATH="$HOME/go"
+fgrep -q "$GOPATH/bin" <<< "$PATH" || export PATH="$PATH:$GOPATH/bin"
 
-export GOPATH=$(pwd)
-export PATH=$PATH:$GOPATH/bin
-
+cd "$GOPATH/src/github.com/rackn/rocket-skates"
 if ! which go &>/dev/null; then
         echo "Must have go installed"
-        exit -1
+        exit 255
 fi
+
 # Work out the GO version we are working with:
 GO_VERSION=$(go version | awk '{ print $3 }' | sed 's/go//')
-GO_VER_MAJOR=""
-GO_VER_MINOR=""
-if [[ "$GO_VERSION" =~ (.*)\.(.*) ]]
-then
-        GO_VER_MAJOR=${BASH_REMATCH[1]}
-        GO_VER_MINOR=${BASH_REMATCH[2]}
+WANTED_VER=(1 8)
+if ! [[ "$GO_VERSION" =~ (.*)\.(.*) ]]; then
+    echo "Cannot figure out what version of Go is installed"
+    exit 1
+elif ! (( ${BASH_REMATCH[1]} > ${WANTED_VER[0]} || ${BASH_REMATCH[2]} >= ${WANTED_VER[1]} )); then
+    echo "Go Version needs to be 1.8 or higher: currently $GO_VERSION"
+    exit -1
 fi
 
-if [[ $GO_VER_MAJOR -ne $GO_TARGET_VER_MAJOR || $GO_VER_MINOR -lt $GO_TARGET_VER_MINOR ]] ; then
-        echo "Go Version needs to be 1.8 or higher: currently $GO_VERSION"
-        exit -1
-fi
-
-if ! which go-bindata ; then
-        go get -u github.com/jteeuwen/go-bindata/...
-fi
-
-if ! which swagger ; then
-        go get -u github.com/go-swagger/go-swagger/cmd/swagger
-fi
-
-if ! which glide ; then
-        go get -v github.com/Masterminds/glide
-        cd $GOPATH/src/github.com/Masterminds/glide && git checkout tags/v0.12.3 && go install && cd -
-fi
-
-if [[ ! -d src/github.com/rackn/rocket-skates ]] ; then
-        go get -v github.com/rackn/rocket-skates
-fi
-
-cd $GOPATH/src/github.com/rackn/rocket-skates
+for tool in go-bindata swagger glide; do
+    which "$tool" &>/dev/null && continue
+    case $tool in
+        go-bindata) go get -u github.com/jteeuwen/go-bindata/...;;
+        swagger)    go get -u github.com/go-swagger/go-swagger/cmd/swagger;;
+        glide)
+            go get -v github.com/Masterminds/glide
+            (cd "$GOPATH/src/github.com/Masterminds/glide" && git checkout tags/v0.12.3 && go install);;
+        *) echo "Don't know how to install $tool"; exit 1;;
+    esac
+done
 
 glide install
-
-./tools/download-assets.sh
+tools/download-assets.sh
 
 go generate server/main.go
 go build -o rocket-skates server/*
 go build -o rscli cli/*
 
 ./tools/test.sh
-
-echo "To rebuild after changes:"
-echo " export GOPATH=$GOPATH"
-echo " export PATH=\$PATH:\$GOPATH/bin"
-echo " cd $GOPATH/src/github.com/rackn/rocket-skates"
-echo " go generate server/main.go"
-echo " go build -o rocket-skates server/*"
-echo " go build -o rscli cli/*"
+echo "To rebuild after changes, rerun tools/build.sh"
