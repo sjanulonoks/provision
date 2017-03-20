e0b8a19c60e2826e56f1081ece8eff8d7008e367
Victor Lowther <victor.lowther@gmail.com>
2017-03-20 16:07:02 -0500

    Expose params via the API

---
+++ frontend/params.go
@@ -0,0 +1,145 @@
+package frontend
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rackn/rocket-skates/backend"
+)
+
+// ParamResponse returned on a successful GET, PUT, PATCH, or POST of a single param
+// swagger:response
+type ParamResponse struct {
+	// in: body
+	Body *backend.Param
+}
+
+// ParamsResponse returned on a successful GET of all the params
+// swagger:response
+type ParamsResponse struct {
+	//in: body
+	Body []*backend.Param
+}
+
+// ParamBodyParameter used to inject a Param
+// swagger:parameters createParam putParam
+type ParamBodyParameter struct {
+	// in: body
+	// required: true
+	Body *backend.Param
+}
+
+// ParamPatchBodyParameter used to patch a Param
+// swagger:parameters patchParam
+type ParamPatchBodyParameter struct {
+	// in: body
+	// required: true
+	Body []JSONPatchOperation
+}
+
+// ParamPathParameter used to name a Param in the path
+// swagger:parameters getParam putParam patchParam deleteParam
+type ParamPathParameter struct {
+	// in: path
+	// required: true
+	Name string `json:"name"`
+}
+
+func (f *Frontend) InitParamApi() {
+	// swagger:route GET /params Params listParams
+	//
+	// Lists Params filtered by some parameters.
+	//
+	// This will show all Params by default.
+	//
+	//     Responses:
+	//       200: ParamsResponse
+	//       401: ErrorResponse
+	f.ApiGroup.GET("/params",
+		func(c *gin.Context) {
+			f.List(c, f.dt.NewParam())
+		})
+
+	// swagger:route POST /params Params createParam
+	//
+	// Create a Param
+	//
+	// Create a Param from the provided object
+	//
+	//     Responses:
+	//       201: ParamResponse
+	//       400: ErrorResponse
+	//       401: ErrorResponse
+	//       422: ErrorResponse
+	f.ApiGroup.POST("/params",
+		func(c *gin.Context) {
+			b := f.dt.NewParam()
+			f.Create(c, b)
+		})
+
+	// swagger:route GET /params/{name} Params getParam
+	//
+	// Get a Param
+	//
+	// Get the Param specified by {name} or return NotFound.
+	//
+	//     Responses:
+	//       200: ParamResponse
+	//       401: ErrorResponse
+	//       404: ErrorResponse
+	f.ApiGroup.GET("/params/:name",
+		func(c *gin.Context) {
+			f.Fetch(c, f.dt.NewParam(), c.Param(`name`))
+		})
+
+	// swagger:route PATCH /params/{name} Params patchParam
+	//
+	// Patch a Param
+	//
+	// Update a Param specified by {name} using a RFC6902 Patch structure
+	//
+	//     Responses:
+	//       200: ParamResponse
+	//       400: ErrorResponse
+	//       401: ErrorResponse
+	//       404: ErrorResponse
+	//       422: ErrorResponse
+	f.ApiGroup.PATCH("/params/:name",
+		func(c *gin.Context) {
+			c.JSON(http.StatusNotImplemented, backend.NewError("API_ERROR", http.StatusNotImplemented, "param patch: NOT IMPLEMENTED"))
+		})
+
+	// swagger:route PUT /params/{name} Params putParam
+	//
+	// Put a Param
+	//
+	// Update a Param specified by {name} using a JSON Param
+	//
+	//     Responses:
+	//       200: ParamResponse
+	//       400: ErrorResponse
+	//       401: ErrorResponse
+	//       404: ErrorResponse
+	//       422: ErrorResponse
+	f.ApiGroup.PUT("/params/:name",
+		func(c *gin.Context) {
+			f.Update(c, f.dt.NewParam(), c.Param(`name`))
+		})
+
+	// swagger:route DELETE /params/{name} Params deleteParam
+	//
+	// Delete a Param
+	//
+	// Delete a Param specified by {name}
+	//
+	//     Responses:
+	//       200: ParamResponse
+	//       401: ErrorResponse
+	//       404: ErrorResponse
+	f.ApiGroup.DELETE("/params/:name",
+		func(c *gin.Context) {
+			b := f.dt.NewParam()
+			b.Name = c.Param(`name`)
+			f.Remove(c, b)
+		})
+}
---
+++ frontend/params_test.go
@@ -0,0 +1,260 @@
+package frontend
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"reflect"
+	"strings"
+	"testing"
+
+	"github.com/digitalrebar/digitalrebar/go/common/store"
+	"github.com/rackn/rocket-skates/backend"
+)
+
+func TestParamList(t *testing.T) {
+	localDTI := testFrontend()
+
+	localDTI.ListValue = nil
+	req, _ := http.NewRequest("GET", "/api/v3/params", nil)
+	w := localDTI.RunTest(req)
+	localDTI.ValidateCode(t, http.StatusOK)
+	localDTI.ValidateContentType(t, "application/json; charset=utf-8")
+
+	var list []backend.Param
+	json.Unmarshal(w.Body.Bytes(), &list)
+	if len(list) != 0 {
+		t.Errorf("Response should be an empty list, but got: %d\n", len(list))
+	}
+
+	localDTI.ListValue = []store.KeySaver{&backend.Param{Name: "fred"}}
+	req, _ = http.NewRequest("GET", "/api/v3/params", nil)
+	w = localDTI.RunTest(req)
+	localDTI.ValidateCode(t, http.StatusOK)
+	localDTI.ValidateContentType(t, "application/json; charset=utf-8")
+
+	json.Unmarshal(w.Body.Bytes(), &list)
+	if len(list) != 1 {
+		t.Errorf("Response should be a list of 1, but got: %d\n", len(list))
+	}
+	if list[0].Name != "fred" {
+		t.Errorf("Response[0] is not named fred, %v\n", list[0].Name)
+	}
+}
+
+func TestParamPost(t *testing.T) {
+	localDTI := testFrontend()
+
+	localDTI.CreateValue = nil
+	localDTI.CreateError = &backend.Error{Code: 23, Messages: []string{"should not get this"}}
+	req, _ := http.NewRequest("POST", "/api/v3/params", nil)
+	req.Header.Set("Content-Type", "text/html")
+	localDTI.RunTest(req)
+	localDTI.ValidateCode(t, http.StatusBadRequest)
+	localDTI.ValidateContentType(t, "application/json; charset=utf-8")
+	localDTI.ValidateError(t, "API_ERROR", "Invalid content type: text/html")
+
+	localDTI.CreateValue = nil
+	localDTI.CreateError = &backend.Error{Code: 23, Messages: []string{"should not get this"}}
+	req, _ = http.NewRequest("POST", "/api/v3/params", strings.NewReader(""))
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+	localDTI.RunTest(req)
+	localDTI.ValidateCode(t, http.StatusBadRequest)
+	localDTI.ValidateContentType(t, "application/json; charset=utf-8")
+	localDTI.ValidateError(t, "API_ERROR", "EOF")
+
+	localDTI.CreateValue = nil
+	localDTI.CreateError = &backend.Error{Code: 23, Messages: []string{"should not get this"}}
+	req, _ = http.NewRequest("POST", "/api/v3/params", strings.NewReader("asgasgd"))
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+	localDTI.RunTest(req)
+	localDTI.ValidateCode(t, http.StatusBadRequest)
+	localDTI.ValidateContentType(t, "application/json; charset=utf-8")
+	localDTI.ValidateError(t, "API_ERROR", "invalid character 'a' looking for beginning of value")
+
+	/* GREG: handle json failure? hard to do - send a machine instead of param */
+
+	localDTI.CreateValue = &backend.Param{Name: "fred", Value: "kfred"}
+	localDTI.CreateError = fmt.Errorf("this is a test: bad fred")
+	v, _ := json.Marshal(localDTI.CreateValue)
+	req, _ = http.NewRequest("POST", "/api/v3/params", strings.NewReader(string(v)))
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+	localDTI.RunTest(req)
+	localDTI.ValidateCode(t, http.StatusBadRequest)
+	localDTI.ValidateContentType(t, "application/json; charset=utf-8")
+	localDTI.ValidateError(t, "API_ERROR", "this is a test: bad fred")
+
+	localDTI.CreateValue = &backend.Param{Name: "fred", Value: "kfred"}
+	localDTI.CreateError = nil
+	v, _ = json.Marshal(localDTI.CreateValue)
+	req, _ = http.NewRequest("POST", "/api/v3/params", strings.NewReader(string(v)))
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+	w := localDTI.RunTest(req)
+	localDTI.ValidateCode(t, http.StatusCreated)
+	localDTI.ValidateContentType(t, "application/json; charset=utf-8")
+
+	var be backend.Param
+	json.Unmarshal(w.Body.Bytes(), &be)
+	if be.Name != "fred" {
+		t.Errorf("Returned Param was not correct: %v %v\n", "fred", be.Name)
+	}
+
+	localDTI.CreateValue = &backend.Param{Name: "fred", Value: "kfred"}
+	localDTI.CreateError = &backend.Error{Code: 23, Messages: []string{"test one"}}
+	v, _ = json.Marshal(localDTI.CreateValue)
+	req, _ = http.NewRequest("POST", "/api/v3/params", strings.NewReader(string(v)))
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+	w = localDTI.RunTest(req)
+	localDTI.ValidateCode(t, 23)
+	localDTI.ValidateContentType(t, "application/json; charset=utf-8")
+	var berr backend.Error
+	json.Unmarshal(w.Body.Bytes(), &berr)
+	if berr.Messages[0] != "test one" {
+		t.Errorf("Returned Error was not correct: %v %v\n", "test one", berr.Messages[0])
+	}
+}
+
+func TestParamGet(t *testing.T) {
+	localDTI := testFrontend()
+
+	localDTI.GetValue = nil
+	localDTI.GetBool = false
+	req, _ := http.NewRequest("GET", "/api/v3/params/fred", nil)
+	localDTI.RunTest(req)
+	localDTI.ValidateCode(t, http.StatusNotFound)
+	localDTI.ValidateContentType(t, "application/json; charset=utf-8")
+	localDTI.ValidateError(t, "API_ERROR", "parameters GET: fred: Not Found")
+
+	localDTI.GetValue = &backend.Param{Name: "fred", Value: "kfred"}
+	localDTI.GetBool = true
+	req, _ = http.NewRequest("GET", "/api/v3/params/fred", nil)
+	w := localDTI.RunTest(req)
+	localDTI.ValidateCode(t, http.StatusOK)
+	localDTI.ValidateContentType(t, "application/json; charset=utf-8")
+	var be backend.Param
+	json.Unmarshal(w.Body.Bytes(), &be)
+	if be.Name != "fred" {
+		t.Errorf("Returned Param was not correct: %v %v\n", "fred", be.Name)
+	}
+}
+
+func TestParamPatch(t *testing.T) {
+	localDTI := testFrontend()
+
+	localDTI.UpdateValue = nil
+	localDTI.UpdateError = nil
+	req, _ := http.NewRequest("PATCH", "/api/v3/params/fred", nil)
+	localDTI.RunTest(req)
+	localDTI.ValidateCode(t, http.StatusNotImplemented)
+	localDTI.ValidateContentType(t, "application/json; charset=utf-8")
+	localDTI.ValidateError(t, "API_ERROR", "param patch: NOT IMPLEMENTED")
+}
+
+func TestParamPut(t *testing.T) {
+	localDTI := testFrontend()
+
+	localDTI.UpdateValue = nil
+	localDTI.UpdateError = &backend.Error{Code: 23, Messages: []string{"should not get this one"}}
+	req, _ := http.NewRequest("PUT", "/api/v3/params/fred", nil)
+	req.Header.Set("Content-Type", "text/html")
+	localDTI.RunTest(req)
+	localDTI.ValidateCode(t, http.StatusBadRequest)
+	localDTI.ValidateContentType(t, "application/json; charset=utf-8")
+	localDTI.ValidateError(t, "API_ERROR", "Invalid content type: text/html")
+
+	localDTI.UpdateValue = nil
+	localDTI.UpdateError = &backend.Error{Code: 23, Messages: []string{"should not get this one"}}
+	req, _ = http.NewRequest("PUT", "/api/v3/params/fred", strings.NewReader(""))
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+	localDTI.RunTest(req)
+	localDTI.ValidateCode(t, http.StatusBadRequest)
+	localDTI.ValidateContentType(t, "application/json; charset=utf-8")
+	localDTI.ValidateError(t, "API_ERROR", "EOF")
+
+	localDTI.UpdateValue = nil
+	localDTI.UpdateError = &backend.Error{Code: 23, Messages: []string{"should not get this one"}}
+	req, _ = http.NewRequest("PUT", "/api/v3/params/fred", strings.NewReader("asgasgd"))
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+	localDTI.RunTest(req)
+	localDTI.ValidateCode(t, http.StatusBadRequest)
+	localDTI.ValidateContentType(t, "application/json; charset=utf-8")
+	localDTI.ValidateError(t, "API_ERROR", "invalid character 'a' looking for beginning of value")
+
+	/* GREG: handle json failure? hard to do - send a machine instead of param */
+
+	localDTI.UpdateValue = &backend.Param{Name: "fred", Value: "kfred"}
+	localDTI.UpdateError = fmt.Errorf("this is a test: bad fred")
+	v, _ := json.Marshal(localDTI.UpdateValue)
+	req, _ = http.NewRequest("PUT", "/api/v3/params/fred", strings.NewReader(string(v)))
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+	localDTI.RunTest(req)
+	localDTI.ValidateCode(t, http.StatusBadRequest)
+	localDTI.ValidateContentType(t, "application/json; charset=utf-8")
+	localDTI.ValidateError(t, "API_ERROR", "this is a test: bad fred")
+
+	localDTI.UpdateValue = &backend.Param{Name: "kfred", Value: "kfred"}
+	localDTI.UpdateError = nil
+	v, _ = json.Marshal(localDTI.UpdateValue)
+	req, _ = http.NewRequest("PUT", "/api/v3/params/fred", strings.NewReader(string(v)))
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+	localDTI.RunTest(req)
+	localDTI.ValidateCode(t, http.StatusBadRequest)
+	localDTI.ValidateContentType(t, "application/json; charset=utf-8")
+	localDTI.ValidateError(t, "API_ERROR", "parameters PUT: Key change from fred to kfred not allowed")
+
+	localDTI.UpdateValue = &backend.Param{Name: "fred", Value: "kfred"}
+	localDTI.UpdateError = &backend.Error{Code: 23, Type: "API_ERROR", Messages: []string{"test one"}}
+	v, _ = json.Marshal(localDTI.UpdateValue)
+	req, _ = http.NewRequest("PUT", "/api/v3/params/fred", strings.NewReader(string(v)))
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+	localDTI.RunTest(req)
+	localDTI.ValidateCode(t, 23)
+	localDTI.ValidateContentType(t, "application/json; charset=utf-8")
+	localDTI.ValidateError(t, "API_ERROR", "test one")
+
+	localDTI.UpdateValue = &backend.Param{Name: "fred", Value: "kfred"}
+	localDTI.UpdateError = nil
+	v, _ = json.Marshal(localDTI.UpdateValue)
+	req, _ = http.NewRequest("PUT", "/api/v3/params/fred", strings.NewReader(string(v)))
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+	w := localDTI.RunTest(req)
+	localDTI.ValidateCode(t, http.StatusOK)
+	localDTI.ValidateContentType(t, "application/json; charset=utf-8")
+	var be backend.Param
+	json.Unmarshal(w.Body.Bytes(), &be)
+	if !reflect.DeepEqual(be.Value, "kfred") {
+		t.Errorf("Returned Param was not correct: %v %v\n", "kfred", be.Value)
+	}
+}
+
+func TestParamDelete(t *testing.T) {
+	localDTI := testFrontend()
+
+	localDTI.RemoveValue = nil
+	localDTI.RemoveError = &backend.Error{Code: 23, Type: "API_ERROR", Messages: []string{"should get this one"}}
+	req, _ := http.NewRequest("DELETE", "/api/v3/params/fred", nil)
+	localDTI.RunTest(req)
+	localDTI.ValidateCode(t, 23)
+	localDTI.ValidateContentType(t, "application/json; charset=utf-8")
+	localDTI.ValidateError(t, "API_ERROR", "should get this one")
+
+	localDTI.RemoveValue = nil
+	localDTI.RemoveError = fmt.Errorf("this is a test: bad fred")
+	req, _ = http.NewRequest("DELETE", "/api/v3/params/fred", nil)
+	localDTI.RunTest(req)
+	localDTI.ValidateCode(t, http.StatusNotFound)
+	localDTI.ValidateContentType(t, "application/json; charset=utf-8")
+	localDTI.ValidateError(t, "API_ERROR", "this is a test: bad fred")
+
+	localDTI.RemoveValue = &backend.Param{Name: "fred", Value: "kfred"}
+	localDTI.RemoveError = nil
+	req, _ = http.NewRequest("DELETE", "/api/v3/params/fred", nil)
+	w := localDTI.RunTest(req)
+	localDTI.ValidateCode(t, http.StatusOK)
+	localDTI.ValidateContentType(t, "application/json; charset=utf-8")
+	var be backend.Param
+	json.Unmarshal(w.Body.Bytes(), &be)
+	if !reflect.DeepEqual(be.Value, "kfred") {
+		t.Errorf("Returned Param was not correct: %v %v\n", "kfred", be.Value)
+	}
+}
