e939f2d28ae773ceb2173a24ac1126b7c3264a4f
Greg Althaus <galthaus@austin.rr.com>
2017-04-01 09:32:38 -0500

    Adjust test tool to generate a slightly better coverage file.

--- tools/test.sh
+++ tools/test.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 set -e
-echo "" > coverage.txt
+echo "mode: atomic" > coverage.txt
 
 for d in $(go list ./... 2>/dev/null | grep -v cmds | grep -v vendor | grep -v github.com/rackn/rocket-skates/client  | grep -v github.com/rackn/rocket-skates/models) ; do
   go test -race -coverprofile=profile.out -covermode=atomic "$d"
   if [ -f profile.out ]; then
-    cat profile.out >> coverage.txt
+    grep -h -v "^mode:" profile.out >> coverage.txt
     rm profile.out
   fi
 done
