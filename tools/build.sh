70bbdd1e0d44dd8e299b7da5187f8dc31a76d677
Greg Althaus <galthaus@austin.rr.com>
2017-04-09 22:24:46 -0500

    More tweaks.

--- tools/build.sh
+++ tools/build.sh
@@ -48,35 +48,36 @@ go generate server/assets.go
 
 TAG=$(git describe --tags --abbrev=1000)
 if [[ $TAG == tip ]] ; then
+    Extra="-tip"
     TAG=$(git describe --tags --abbrev=1000 HEAD^2)
 fi
 
 tag_re='([^-]+)-([^-]+)-g([^ ]+)'
 semver_re='v([0-9]+).([0-9]+).([0-9]+)'
 if [[ $TAG =~ $tag_re ]]; then
-            BASE="${BASH_REMATCH[1]}"
-            AHEAD="${BASH_REMATCH[2]}"
-            GITHASH="${BASH_REMATCH[3]}"
+    BASE="${BASH_REMATCH[1]}"
+    AHEAD="${BASH_REMATCH[2]}"
+    GITHASH="${BASH_REMATCH[3]}"
 fi
 
 if [[ $BASE == tip ]] ; then
-        MajorV="tip"
-        MinorV=$(whoami)
-        PatchV=$AHEAD
-        Extra="-dev"
-        Prepart=""
+    MajorV="tip"
+    MinorV=$(whoami)
+    PatchV=$AHEAD
+    Extra="-tip"
+    Prepart=""
 elif [[ $BASE =~ $semver_re ]] ; then
-        MajorV=${BASH_REMATCH[1]}
-        MinorV=${BASH_REMATCH[2]}
-        PatchV=${BASH_REMATCH[3]}
-        Extra="-$AHEAD"
-        Prepart="v"
+    MajorV=${BASH_REMATCH[1]}
+    MinorV=${BASH_REMATCH[2]}
+    PatchV=${BASH_REMATCH[3]}
+    Extra="$Extra-$AHEAD"
+    Prepart="v"
 else
-        MajorV="$BASE"
-        MinorV=$(whoami)
-        PatchV=$AHEAD
-        Extra="-strange"
-        Prepart=""
+    MajorV="$BASE"
+    MinorV=$(whoami)
+    PatchV=$AHEAD
+    Extra="$Extra-strange"
+    Prepart=""
 fi
 
 echo "Version = $Prepart$MajorV.$MinorV.$PatchV$Extra-$GITHASH"
