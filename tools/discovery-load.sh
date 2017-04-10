2a6f62330f5ff9031662b4d49937925566b15622
Greg Althaus <galthaus@austin.rr.com>
2017-04-10 11:33:28 -0500

    Add pref changes required to use discovery is load discovery is used.

--- tools/discovery-load.sh
+++ tools/discovery-load.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-export RS_KEY=rocketskates:r0cketsk8ts
+export RS_KEY=${RS_KEY:-rocketskates:r0cketsk8ts}
 ./drpcli bootenvs install bootenvs/local.yml
 ./drpcli bootenvs install bootenvs/discovery.yml
 ./drpcli bootenvs install bootenvs/sledgehammer.yml
+
+./drpcli set prefs unknownBootEnv discovery defaultBootEnv sledgehammer
 
