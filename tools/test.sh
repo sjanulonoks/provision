9ef2a5b42b58b8cca0913b72e9ebd7967185b2d6
Victor Lowther <victor.lowther@gmail.com>
2017-05-04 13:32:36 -0500

    Fix test runner to handle nested packages

--- tools/test.sh
+++ tools/test.sh
@@ -5,6 +5,7 @@ echo "mode: atomic" > coverage.txt
 
 packages="github.com/digitalrebar/provision,\
 github.com/digitalrebar/provision/backend,\
+github.com/digitalrebar/provision/backend/index,\
 github.com/digitalrebar/provision/midlayer,\
 github.com/digitalrebar/provision/frontend,\
 github.com/digitalrebar/provision/embedded,\
@@ -13,13 +14,12 @@ github.com/digitalrebar/provision/cli\
 "
 
 for d in $(go list ./... 2>/dev/null | grep -v cmds | grep -v vendor | grep -v github.com/digitalrebar/provision/client  | grep -v github.com/digitalrebar/provision/models) ; do
+    tdir=$PWD
     dir=${d//github.com\/digitalrebar\/provision}
     rm -f test.bin
     go test -o test.bin -c -race -covermode=atomic -coverpkg=$packages "$d"
     if [ -e test.bin ] ; then
-        cd .$dir
-        time ../test.bin -test.coverprofile=../profile.out
-        cd ..
+        (cd .$dir; time "$tdir/test.bin" -test.coverprofile="$tdir/profile.out")
         rm -f test.bin
     fi
 
