e45268cd43d935e29ef855b83cf0422d23337643
Shane Gibson <shane@zerostack.com>
2017-09-23 20:07:21 -0700

    ver3 updates to 5min-drp

--- examples/5min-drp/drp-endpoint.tf
+++ examples/5min-drp/drp-endpoint.tf
@@ -1,15 +1,11 @@
 
-//provider "packet" {
-//    auth_token = "${var.packet_api_key}"
-//}
-
-resource "packet_ssh_key" "5min-drp" {
-  name       = "5min-drp-demo-key"
+resource "packet_ssh_key" "5min-drp-ssh-key" {
+  name       = "5min-drp-ssh-key"
   public_key = "${file("${var.drp_ssh_public_key_path}")}"
 }
 
 resource "packet_device" "5min-drp" {
-  hostname         = "${format("drp-${var.packet_facility}-%02d", count.index)}"
+  hostname         = "${format("5min-drp-${var.packet_facility}-%02d", count.index)}"
   operating_system = "centos_7"
   plan             = "${var.endpoint_type}"
   facility         = "${var.packet_facility}"
--- examples/5min-drp/drp-nodes.tf
+++ examples/5min-drp/drp-nodes.tf
@@ -4,37 +4,28 @@
 // block, we have to hard-wire our DRP Endpoint in to this
 //
 
-resource "packet_ssh_key" "5min-nodes" {
-  name       = "5min-nodes-demo-key"
+// NOTICE NOTICE NOTICE 
+//
+// we rely on the drp-endpoint being fully provisioned first, and the IP Address
+// and port of the endpoint will be injected in to this file, replacing the 
+// 'drp_endpoint_address_and_port' value
+///
+
+resource "packet_ssh_key" "5min-nodes-ssh-key" {
+  name       = "5min-nodes-ssh-key"
   public_key = "${file("${var.nodes_ssh_public_key_path}")}"
 }
 
 variable "node_data" {
 default =<<EOF
 #!ipxe
-echo mac...............: $${mac}
-echo ip................: $${ip}
-echo netmask...........: $${netmask}
-echo gateway...........: $${gateway}
-echo dns...............: $${dns}
-echo domain............: $${domain}
-echo dhcp-server.......: $${dhcp-server}
-echo syslog............: $${syslog}
-echo filename..........: $${filename}
-echo next-server.......: $${next-server}
-echo hostname..........: $${hostname}
-echo uuid..............: $${uuid}
-echo serial............: $${serial}
-echo .
-chain http://147.75.108.41:8091/default.ipxe
+chain http://drp_endpoint_address_and_port/default.ipxe
 EOF
-//kernel $${base-url}/vmlinuz dhcp modprobe.blacklist=ixgbe boot=live fetch=$${base-url}/filesystem.squashfs console=tty0 console=ttyS1,115200
-//initrd $${base-url}/initrd.img
-//boot
+// example: chain http://147.75.108.41:8091/default.ipxe
 }
 
 resource "packet_device" "5min-nodes" {
-  hostname         = "${format("nodes-${var.packet_facility}-${var.cluster_name}-%02d", count.index + 1)}"
+  hostname         = "${format("${var.cluster_name}-nodes-${var.packet_facility}-%02d", count.index + 1)}"
   operating_system = "custom_ipxe"
   count            = "${var.node_count}"
   plan             = "${var.node_type}"
