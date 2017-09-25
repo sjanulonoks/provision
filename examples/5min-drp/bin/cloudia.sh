fea75a0a112b6a2f9b1355e8b2b7cfd8b59795c8
Shane Gibson <shane@zerostack.com>
2017-09-25 11:33:32 -0700

    .gitignore eccentricities - force adding scripts

---
+++ examples/5min-drp/bin/cloudia.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+clear
+cat <<'CLOUDIA2'
+
+     `/yy/.                                              `/yhs:           
+   ./syhmNNs-                                          `/mMMNdy+`         
+  :so:-:oymMNh:     `-://+oossyyyyyysssoo+/:-.`       `sMMNhyo//o`        
+  +s-   `:oymMNh/ohmNNNMMMMMMMMMMMMMMMMMMMMMNNNdyo/.   `-+oo-` `o/        
+  `:o:`   `/ymMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNdo-`  ``  .//`        
+    ./o-``:hMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNd/`   -:.`         
+     `./shNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy`  `            
+       :mMMMMMMMMMMmhyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyhmMMMy              
+      `oyMMMMMMMMMy-```````````````````````````   ````.-hMMM/             
+      .ssdMMMMMMMMmmmmmmmdyosdmmmmmmmmmmmmmmmmmo` -ymmmmmMMMm.            
+      -ssymMMMMMMMMMMMMNhys+syhNMMMMMMMMMMMMMMMMd/``+mMMMMMMMo            
+     `ossssydMMMMMMMNhssss//sssssyhmMMMMMMMMMMMMMMMNs. -hMMMMm:           
+     `ssssssshNMMMMNyssss+//sssssssyhNMMMMMMMMMMMMMMNd: `hMMmyo           
+     .ssssssssydNMNysssso///sssssssssyyyo+++++++++++++o  +Ndyss.          
+     .ssssssssssyhysssss+///ssssssssssss/`           -o  :yssss:          
+     -ssssssssssssssssss////sssssssssssss/`         .oo  -sssss+          
+     -sssssssssssssssss+////ssssssssssssss:        `+so  -ssssso          
+     .sssssssssssssssss/////sssssssssssssss:```````:sso  -ssssso`         
+     `ossssssssssssssss/////ssssssssssssssssooooooossso  -ssssso`         
+        `-osssssssssss+/////++++++++++++++++++++++++++:  -/++/.           
+          `-+sssssssss+//////:------------------------.  -/-`             
+             `:+sssssso////:.````````````````````````                     
+                `:+ossso///::::::::::::::::::::://+/-`                    
+                   `-/oss+//////////////////++oo/-`                       
+                       `-/oo++//////////++oo/-.                           
+                           `-/+oo+//++oo/-.                               
+                               `.:/+/-`                                  
+
+CLOUDIA2
+
+exit 0
