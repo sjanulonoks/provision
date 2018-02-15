855e92f6414a0860394280fa674bd5522a3af2db
Greg Althaus <galthaus@austin.rr.com>
2018-02-14 20:29:48 -0600

    Don't notify docker hub because it sucks.

--- tools/after_deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -H "Content-Type: application/json" --data "{\"source_type\": \"Tag\", \"source_name\": \"$TRAVIS_TAG\"}" -X POST https://registry.hub.docker.com/u/digitalrebar/provision/trigger/f3e8b588-59d7-43f7-b855-b49019de2dee/
